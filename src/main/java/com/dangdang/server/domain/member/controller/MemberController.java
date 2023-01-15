9b22deeaa4056be72322ef4d777856526e364610
cloudwi <cloudwi@naver.com>
2023-01-15 20:00:18 +0900

    feat : [TRYC-21] 휴대폰 인증 추가
    - redis를 활용함
    - 사용자의 핸드폰 번호 + 인증코드를 관리함

--- src/main/java/com/dangdang/server/domain/member/controller/MemberController.java
+++ src/main/java/com/dangdang/server/domain/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.dangdang.server.domain.member.controller;
 
-import com.dangdang.server.domain.member.dto.MemberCertifiedRequestDto;
+import com.dangdang.server.domain.member.dto.request.MemberCertifiyRequestDto;
+import com.dangdang.server.domain.member.dto.request.MemberSendRequestDto;
+import com.dangdang.server.domain.member.service.MemberCertifiedService;
 import com.dangdang.server.domain.member.service.SmsMessageService;
 import javax.validation.Valid;
 import javax.validation.constraints.Null;
@@ -15,15 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
   private final SmsMessageService smsMessageService;
+  private final MemberCertifiedService memberCertifiedService;
 
-  public MemberController(SmsMessageService smsMessageService) {
+  public MemberController(SmsMessageService smsMessageService,
+      MemberCertifiedService memberCertifiedService) {
     this.smsMessageService = smsMessageService;
+    this.memberCertifiedService = memberCertifiedService;
   }
 
   @PostMapping("/send")
   public ResponseEntity<Null> send(
-      @RequestBody @Valid MemberCertifiedRequestDto memberCertifiedRequestDto) {
-    smsMessageService.sendMessage(memberCertifiedRequestDto.getToNumber());
+      @RequestBody @Valid MemberSendRequestDto memberSendRequestDto) {
+    smsMessageService.sendMessage(memberSendRequestDto.getToNumber());
+
+    return ResponseEntity.ok(null);
+  }
+
+  @PostMapping("/certify")
+  public ResponseEntity<Null> certify(
+      @RequestBody @Valid MemberCertifiyRequestDto memberCertifiyRequestDto) {
+    memberCertifiedService.certify(memberCertifiyRequestDto.getToNumber(),
+        memberCertifiyRequestDto.getRandomNumber());
+
     return ResponseEntity.ok(null);
   }
 }
--- src/main/java/com/dangdang/server/domain/member/service/SmsMessageService.java
+++ src/main/java/com/dangdang/server/domain/member/service/SmsMessageService.java
@@ -1,8 +1,8 @@
 package com.dangdang.server.domain.member.service;
 
+import com.dangdang.server.global.utill.RedisUtil;
 import java.util.Random;
 import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.exception.NurigoException;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
@@ -14,20 +14,26 @@ import org.springframework.stereotype.Service;
 public class SmsMessageService {
 
   private String fromNumber;
-
+  private final RedisUtil redisUtil;
   private final DefaultMessageService defaultMessageService;
+  private Random random = new Random();
 
   public SmsMessageService(@Value("${secret.coolsms.apikey}") String apiKey,
       @Value("${secret.coolsms.apiSecret}") String apiSecret,
-      @Value("${secret.coolsms.fromNumber}") String fromNumber) {
+      @Value("${secret.coolsms.fromNumber}") String fromNumber,
+      RedisUtil redisUtil) {
     this.fromNumber = fromNumber;
+    this.redisUtil = redisUtil;
     this.defaultMessageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret,
         "https://api.coolsms.co.kr");
   }
 
   public SingleMessageSentResponse sendMessage(String toNumber) {
     //redis에 넣어야함
-    int code = generateAuthNo2();
+    String code = generateAuthNo2();
+
+    redisUtil.deleteData(toNumber);
+    redisUtil.setDataExpire(toNumber, code, 60 * 5L);
 
     String text =
         "[Web 발신]\n" + "[당근마켓] 인증번호 [" + code + "] *타인에게 절대 알리지 마세요. (계정 도용 위험)";
@@ -37,15 +43,11 @@ public class SmsMessageService {
     message.setTo(toNumber);
     message.setText(text);
 
-    SingleMessageSentResponse singleMessageSentResponse =
-        defaultMessageService.sendOne(new SingleMessageSendingRequest(message));
-
-    return singleMessageSentResponse;
+    return defaultMessageService.sendOne(new SingleMessageSendingRequest(message));
   }
 
-  public int generateAuthNo2() {
-    Random generator = new Random();
-    generator.setSeed(System.currentTimeMillis());
-    return generator.nextInt(1000000) % 1000000;
+  public String generateAuthNo2() {
+    random.setSeed(System.currentTimeMillis());
+    return String.valueOf(random.nextInt(1000000) % 1000000);
   }
 }

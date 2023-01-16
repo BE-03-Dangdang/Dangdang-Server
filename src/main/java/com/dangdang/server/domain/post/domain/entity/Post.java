f8f16c59dd47cb47a36caf61fddf58429067f341
cloudwi <cloudwi@naver.com>
2023-01-16 17:32:32 +0900

    feat : [TRYC-113] 패키지 구조 수정

--- src/main/java/com/dangdang/server/domain/post/domain/entity/Post.java
+++ src/main/java/com/dangdang/server/domain/post/domain/entity/Post.java
@@ -1,11 +1,24 @@
 package com.dangdang.server.domain.post.domain.entity;
 
 import com.dangdang.server.domain.common.BaseEntity;
+import com.dangdang.server.domain.member.domain.entity.Member;
+import com.dangdang.server.domain.post.domain.Category;
+import com.dangdang.server.domain.post.dto.request.PostFindAllRequest;
+import com.dangdang.server.domain.town.domain.entity.Town;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import jdk.jfr.Unsigned;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -16,7 +29,45 @@ public class Post extends BaseEntity {
   @Column(name = "post_id")
   private Long id;
 
+  @Column(nullable = false, length = 255)
+  private String title;
+
+  @Column(nullable = false, length = 1000)
+  private String content;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Category category;
+
+  @Column(columnDefinition = "INT UNSIGNED")
+  private Integer price;
+
+  @Column(length = 100)
+  private String desiredPlaceName;
+
+  @Column(precision = 18, scale = 10)
+  private BigDecimal desiredPlaceLongitude;
+
+  @Column(precision = 18, scale = 10)
+  private BigDecimal desiredPlaceLatitude;
+
+  @Column(nullable = false)
+  @ColumnDefault("0")
+  private Integer view;
+
+  @Column(nullable = false)
+  @ColumnDefault("false")
+  private Boolean sharing = false;
+
+  //연관관계
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "town_id")
+  private Town town;
+
   protected Post() {
   }
-
 }
--- src/main/java/com/dangdang/server/domain/review/domain/entity/Review.java
+++ src/main/java/com/dangdang/server/domain/review/domain/entity/Review.java
@@ -1,10 +1,15 @@
 package com.dangdang.server.domain.review.domain.entity;
 
 import com.dangdang.server.domain.common.BaseEntity;
+import com.dangdang.server.domain.member.domain.entity.Member;
+import com.dangdang.server.domain.post.domain.entity.Post;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
@@ -15,6 +20,27 @@ public class Review extends BaseEntity {
   @GeneratedValue
   @Column(name = "review_id")
   private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "reviewer_id")
+  private Member reviewerId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "reviewee_id")
+  private Member revieweeId;
+
+  @Column(nullable = false, length = 100)
+  private String preference;
+
+  @Column(nullable = false, name = "nice_point", length = 100)
+  private String nicePoint;
+
+  @Column(length = 1000)
+  private String content;
 
   protected Review() {
   }

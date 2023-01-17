32c16e453ba2be242a2879a4c58262c5c5a4f4b5
ybkim-dev <beanybigggy@gmail.com>
2023-01-17 18:49:58 +0900

    fix : [TRYC-127] Post Entity id 전략 수정
    - Post Entity id 전략 변경
    - Table id 속성값을 Auto increment로 변경하면서 Post Entity id 전략을 변경합니다.

--- src/main/java/com/dangdang/server/domain/post/domain/entity/Post.java
+++ src/main/java/com/dangdang/server/domain/post/domain/entity/Post.java
@@ -11,6 +11,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import org.hibernate.annotations.ColumnDefault;
 public class Post extends BaseEntity {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "post_id")
   private Long id;
 

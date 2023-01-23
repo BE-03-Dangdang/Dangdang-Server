f86c54d25d7f4ea7b7b695f00d8e6768aeea43c1
ybkim-dev <beanybigggy@gmail.com>
2023-01-23 14:11:15 +0900

    feat : S3 Util 메서드 (사진 조회 링크 제공) 제공
    - S3의 보안체계는 권한에 따라 링크를 열 수 없는 문제가 있습니다.
    - 현재 저희의 IAM 보안 단계에서는 모든 사용자가 링크를 통해 이미지를 조회할 수 없습니다.
    - 따라서, 1시간 동안 이미지를 열 수 있는 링크를 만들어 제공하는 기능을 작성하였습니다.

--- src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
+++ src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
@@ -6,7 +6,9 @@ public enum ExceptionCode {
   CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 카테고리가 존재하지 않습니다."),
   TOWN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 도시가 존재하지 않습니다."),
   POST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 게시글이 존재하지 않습니다."),
-  BINDING_WRONG(HttpStatus.BAD_REQUEST.value(), "요청하신 필드값의 유효성이 잘못되었습니다.");
+  BINDING_WRONG(HttpStatus.BAD_REQUEST.value(), "요청하신 필드값의 유효성이 잘못되었습니다."),
+  IMAGE_URL_INVALID(HttpStatus.BAD_REQUEST.value(), "이미지 주소가 잘못되었습니다.");
+
 
   int status;
   String message;

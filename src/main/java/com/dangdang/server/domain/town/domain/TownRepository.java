594258e9f4f8988274a4b4ba9cbafeb39e768b67
yeeeze <vnqvnq11@naver.com>
2023-01-24 16:45:23 +0900

    Update TownRepository.java
    - TownRepository 중복파일 삭제된 내용 합침
    - 테스트 실패로 인한 임시 메서드 주석 처리

--- src/main/java/com/dangdang/server/domain/town/domain/TownRepository.java
+++ src/main/java/com/dangdang/server/domain/town/domain/TownRepository.java
@@ -11,5 +11,7 @@ public interface TownRepository extends JpaRepository<Town, Long> {
     @description : 게시글 전체 조회 로직에서 조회 대상이 될 동 id를 가져오기 위해 임시로 작성한 메서드입니다.
     실제 동작하지 않으므로 편의에 맞게 수정해주세요. 인터페이스만 만들어둔 것입니다.
    */
-  public List<Long> findAdjacencyTownIdByRangeTypeAndTownId(long townId, int range);
+//  public List<Long> findAdjacencyTownIdByRangeTypeAndTownId(long townId, int range);
+  
+    Optional<Town> findByName(String townName);
 }

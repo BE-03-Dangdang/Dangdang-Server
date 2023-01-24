3182e6766597be09048e8fd74be17dc9b3943955
yeeeze <vnqvnq11@naver.com>
2023-01-24 20:54:42 +0900

    Update ExceptionCode.java
    - 오타 수정

--- src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
+++ src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
@@ -14,7 +14,7 @@ public enum ExceptionCode {
   TRUST_ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "신탁 계좌를 찾지 못했습니다."),
   TRUST_ACCOUNT_INACTIVE(HttpStatus.BAD_REQUEST.value(), "사용할 수 없는 신탁계좌입니다."),
   BANK_ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "은행 계좌를 찾지 못했습니다."),
-  BANK_ACCOUNT_INACTIVE(HttpStatus.BAD_REQUEST.value(), "사용할 수 없는 은은향계좌입니다."),
+  BANK_ACCOUNT_INACTIVE(HttpStatus.BAD_REQUEST.value(), "사용할 수 없는 은행계좌입니다."),
   BANK_ACCOUNT_AUTHENTICATION_FAIL(HttpStatus.UNAUTHORIZED.value(), "은행계좌의 인증에 실패했습니다."),
   PAY_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "당근페이 유저를 찾지 못했습니다."),
   LESS_THAN_MIN_AMOUNT(HttpStatus.BAD_REQUEST.value(), "최소 충전금액은 10,000원입니다."),

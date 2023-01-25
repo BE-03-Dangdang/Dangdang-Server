567cd32ad23c3029657adc9e69f9c0deb7f8d450
cloudwi <cloudwi@naver.com>
2023-01-25 12:35:18 +0900

    feat : BindingResult bindingResult 도입
    - @Valid로 잡은 예외들을 정확하게 클라이언트로 알려주면 보안에 취약 따라서 모호하게 잡아서 클라이언트에게 전달하기 위함

--- src/main/java/com/dangdang/server/controller/member/MemberController.java
+++ src/main/java/com/dangdang/server/controller/member/MemberController.java
@@ -4,8 +4,11 @@ import com.dangdang.server.domain.member.application.MemberService;
 import com.dangdang.server.domain.member.dto.request.MemberSignUpRequest;
 import com.dangdang.server.domain.member.dto.request.PhoneNumberCertifyRequest;
 import com.dangdang.server.domain.member.dto.response.MemberCertifyResponse;
+import com.dangdang.server.global.exception.BusinessException;
+import com.dangdang.server.global.exception.ExceptionCode;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,15 +33,26 @@ public class MemberController {
 
   @PostMapping("/signup-certify")
   public ResponseEntity<MemberCertifyResponse> signupCertify(
-      @RequestBody @Valid PhoneNumberCertifyRequest phoneNumberCertifyRequest) {
-    MemberCertifyResponse memberCertifyResponse = memberService.signupCertify(phoneNumberCertifyRequest);
+      @RequestBody @Valid PhoneNumberCertifyRequest phoneNumberCertifyRequest,
+      BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      throw new BusinessException(ExceptionCode.BAD_REQUEST);
+    }
+
+    MemberCertifyResponse memberCertifyResponse = memberService.signupCertify(
+        phoneNumberCertifyRequest);
 
     return ResponseEntity.ok(memberCertifyResponse);
   }
 
   @PostMapping("/login-certify")
   public ResponseEntity<MemberCertifyResponse> loginCertify(
-      @RequestBody @Valid PhoneNumberCertifyRequest phoneNumberCertifyRequest) {
+      @RequestBody @Valid PhoneNumberCertifyRequest phoneNumberCertifyRequest,
+      BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      throw new BusinessException(ExceptionCode.BAD_REQUEST);
+    }
+
     MemberCertifyResponse memberCertifyResponse = memberService.loginCertify(
         phoneNumberCertifyRequest);
 

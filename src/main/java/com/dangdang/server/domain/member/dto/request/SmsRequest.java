944aba27863a7513a03b5e125ad9093fe27a4627
cloudwi <cloudwi@naver.com>
2023-01-25 12:41:08 +0900

    feat : SmsRequest 검증 로직 개발

--- src/main/java/com/dangdang/server/domain/member/dto/request/SmsRequest.java
+++ src/main/java/com/dangdang/server/domain/member/dto/request/SmsRequest.java
@@ -1,11 +1,15 @@
 package com.dangdang.server.domain.member.dto.request;
 
 import com.dangdang.server.domain.member.domain.entity.RedisSms;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SmsRequest {
 
+  @NotBlank
+  @Pattern(regexp = "[\\d]{11}", message = "핸드폰 번호 또는 인증코드가 잘못되었습니다.")
   private String toPhoneNumber;
 
   private SmsRequest() {

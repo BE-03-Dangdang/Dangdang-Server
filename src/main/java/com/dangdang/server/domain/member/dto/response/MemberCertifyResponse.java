bf81727116844b9f796df551e28936e213f44263
cloudwi <cloudwi@naver.com>
2023-01-25 12:34:16 +0900

    feat : MemberCertifyResponse -> record 형식으로 변경
    - 정적매서드를 활용해서 생성자 가독성 향상

--- src/main/java/com/dangdang/server/domain/member/dto/response/MemberCertifyResponse.java
+++ src/main/java/com/dangdang/server/domain/member/dto/response/MemberCertifyResponse.java
@@ -3,17 +3,12 @@ package com.dangdang.server.domain.member.dto.response;
 import lombok.Getter;
 
 @Getter
-public class MemberCertifyResponse {
+public record MemberCertifyResponse (
+    String accessToken,
+    Boolean isCertified
+) {
 
-  private String accessToken;
-  private Boolean isCertified;
-
-  private MemberCertifyResponse() {
-
-  }
-
-  public MemberCertifyResponse(String accessToken, Boolean isCertified) {
-    this.accessToken = accessToken;
-    this.isCertified = isCertified;
+  public static MemberCertifyResponse from(String accessToken, Boolean isCertified) {
+    return new MemberCertifyResponse(accessToken, isCertified);
   }
 }

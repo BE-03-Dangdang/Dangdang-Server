66356b15bea2d9b858a7ed0486372efbfa1182df
yeezze <yeeeze22@gmail.com>
2023-01-25 21:07:39 +0900

    refactor: Pay 인증실패 http status 변경
    - UNAUTHORIZED -> BAD_REQUEST

--- src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
+++ src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public enum ExceptionCode {
   CERTIFIED_FAIL(HttpStatus.UNAUTHORIZED.value(), "인증에 실패히였습니다"),
-  TOWN_NOT_FOUND(HttpStatus.NOT_FOUND.value(),"타운을 찾지 못하였습니다"),
+  TOWN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "타운을 찾지 못하였습니다"),
   MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "멤버를 찾지 못하였습니다"),
   MEMBER_TOWN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "멤버 타운을 찾지 못하였습니다"),
   NOT_APPROPRIATE_COUNT(HttpStatus.BAD_REQUEST.value(), "허용 가능한 갯수가 아닙니다"),
@@ -16,7 +16,7 @@ public enum ExceptionCode {
   TRUST_ACCOUNT_INACTIVE(HttpStatus.BAD_REQUEST.value(), "사용할 수 없는 신탁계좌입니다."),
   BANK_ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "은행 계좌를 찾지 못했습니다."),
   BANK_ACCOUNT_INACTIVE(HttpStatus.BAD_REQUEST.value(), "사용할 수 없는 은행계좌입니다."),
-  BANK_ACCOUNT_AUTHENTICATION_FAIL(HttpStatus.UNAUTHORIZED.value(), "은행계좌의 인증에 실패했습니다."),
+  BANK_ACCOUNT_AUTHENTICATION_FAIL(HttpStatus.BAD_REQUEST.value(), "은행계좌의 인증에 실패했습니다."),
   PAY_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "당근페이 유저를 찾지 못했습니다."),
   CHARGE_LESS_THAN_MIN_AMOUNT(HttpStatus.BAD_REQUEST.value(), "최소 충전금액은 10,000원입니다."),
   WITHDRAW_LESS_THAN_MIN_AMOUNT(HttpStatus.BAD_REQUEST.value(), "최소 출금금액은 1원입니다."),

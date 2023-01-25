3411591dbf27d699fccc87fbe55da7daf4e8593d
cloudwi <cloudwi@naver.com>
2023-01-25 12:40:48 +0900

    feat : RedisSendSms 객체 완성

--- src/main/java/com/dangdang/server/domain/member/domain/entity/RedisSendSms.java
+++ src/main/java/com/dangdang/server/domain/member/domain/entity/RedisSendSms.java
@@ -3,15 +3,21 @@ package com.dangdang.server.domain.member.domain.entity;
 import javax.persistence.Id;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 @Getter
 @RedisHash(value = "redisSendSms")
 public class RedisSendSms {
 
+  public static final Long SEND_SMS_TTL = 10L;
+
   @Id
   private String id;
+  @TimeToLive
+  private Long expiration;
 
-  public RedisSendSms(String phoneNumber) {
-    this.id = phoneNumber;
+  public RedisSendSms(String id) {
+    this.id = id;
+    this.expiration = SEND_SMS_TTL;
   }
 }

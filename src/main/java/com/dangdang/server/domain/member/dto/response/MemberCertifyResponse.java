5d642a45ea4c7f6308029a9a17d03cf2e68cb663
cloudwi <cloudwi@naver.com>
2023-01-26 13:26:16 +0900

    feat : MemberCertifyResponse에 refreshToken 추가

--- src/main/java/com/dangdang/server/domain/member/dto/response/MemberCertifyResponse.java
+++ src/main/java/com/dangdang/server/domain/member/dto/response/MemberCertifyResponse.java
@@ -1,14 +1,12 @@
 package com.dangdang.server.domain.member.dto.response;
 
-import lombok.Getter;
-
-@Getter
 public record MemberCertifyResponse (
     String accessToken,
+    String refreshToken,
     Boolean isCertified
 ) {
 
-  public static MemberCertifyResponse from(String accessToken, Boolean isCertified) {
-    return new MemberCertifyResponse(accessToken, isCertified);
+  public static MemberCertifyResponse from(String accessToken, String refreshToken ,Boolean isCertified) {
+    return new MemberCertifyResponse(accessToken, refreshToken, isCertified);
   }
 }

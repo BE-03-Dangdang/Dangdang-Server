5a0838d6b995406fdef83d6d60d09082d0643668
cloudwi <cloudwi@naver.com>
2023-01-26 18:31:17 +0900

    feat : 인증 실패 status code 변경

--- src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
+++ src/main/java/com/dangdang/server/global/exception/ExceptionCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
 
   CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 카테고리가 존재하지 않습니다."),
-  CERTIFIED_FAIL(HttpStatus.BAD_REQUEST.value(), "인증에 실패히였습니다"),
+  CERTIFIED_FAIL(HttpStatus.NOT_FOUND.value(), "인증에 실패히였습니다"),
   TOWN_NOT_FOUND(HttpStatus.NOT_FOUND.value(),"타운을 찾지 못하였습니다"),
   BINDING_WRONG(HttpStatus.BAD_REQUEST.value(), "요청하신 필드값의 유효성이 잘못되었습니다."),
   IMAGE_URL_INVALID(HttpStatus.BAD_REQUEST.value(), "이미지 주소가 잘못되었습니다."),

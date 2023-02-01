3a4fc83aaada37d5025f3fb25533f4a710e403bb
kivv00ng <rldnd2637@naver.com>
2023-02-01 17:41:30 +0900

    refactor : 로그인 유저와 리뷰작성자가 일치하는지 확인하는 기능 추가

--- src/main/java/com/dangdang/server/controller/ReviewController.java
+++ src/main/java/com/dangdang/server/controller/ReviewController.java
@@ -1,10 +1,15 @@
 package com.dangdang.server.controller;
 
+import static com.dangdang.server.global.exception.ExceptionCode.MEMBER_UNMATCH_AUTHOR;
+
+import com.dangdang.server.domain.member.domain.entity.Member;
+import com.dangdang.server.domain.member.exception.MemberUnmatchedAuthorException;
 import com.dangdang.server.domain.review.application.ReviewService;
 import com.dangdang.server.domain.review.dto.ReviewRequest;
 import com.dangdang.server.domain.review.dto.ReviewResponse;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +24,19 @@ public class ReviewController {
   }
 
   @PostMapping("/reviews")
-  public ResponseEntity<ReviewResponse> saveReview(@RequestBody ReviewRequest reviewRequest) {
+  public ResponseEntity<ReviewResponse> saveReview(@RequestBody ReviewRequest reviewRequest,
+      Authentication authentication) {
+
+    Long memberId = ((Member) authentication.getPrincipal()).getId();
+    if (memberId == reviewRequest.reviewerId()) {
+      throw new MemberUnmatchedAuthorException(MEMBER_UNMATCH_AUTHOR);
+    }
+
     ReviewResponse reviewResponse = reviewService.saveReview(reviewRequest);
     return ResponseEntity.created(
-            URI.create("/" + reviewResponse.reviewee().memberId() + "/" + reviewResponse.reviewId()))
+            URI.create(
+                "/" + reviewResponse.reviewer().memberId() + "/reviews-sent/"
+                    + reviewResponse.reviewId()))
         .body(reviewResponse);
   }
 }

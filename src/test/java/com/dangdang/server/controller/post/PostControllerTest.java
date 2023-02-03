cb0817300f83f374f50193627ad756d3ff03bb33
ybkim-dev <beanybigggy@gmail.com>
2023-02-03 16:43:17 +0900

    fix : Post Controller 테스트 코드 수정

--- src/test/java/com/dangdang/server/controller/post/PostControllerTest.java
+++ src/test/java/com/dangdang/server/controller/post/PostControllerTest.java
@@ -119,7 +119,7 @@ class PostControllerTest {
   @DisplayName("사용자는 판매중, 예약중, 판매완료 중 1개의 상태로 post의 상태를 변경할 수 있다.")
   void updatePostStatus(String status) throws Exception {
 
-    mockMvc.perform(patch("/posts/status/" + postId).contentType(MediaType.APPLICATION_JSON)
+    mockMvc.perform(patch("/posts/" + postId + "/status").contentType(MediaType.APPLICATION_JSON)
             .header("AccessToken", accessToken).content(objectMapper.writeValueAsString(
                 new PostUpdateStatusRequest(StatusType.valueOf(status))))).andDo(print())
         .andExpect(status().isOk()).andDo(
@@ -302,44 +302,6 @@ class PostControllerTest {
   }
 
   @Test
-  @DisplayName("게시글 상세 정보를 확인할 수 있다.")
-  void findPostDetailTest() throws Exception {
-
-    mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{id}", postId)
-            .header("AccessToken", accessToken))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-        .andDo(print()).andDo(document("post/api/get/findById", preprocessResponse(prettyPrint()),
-            requestHeaders(headerWithName("AccessToken").description("Access Token")),
-            responseFields(
-                fieldWithPath("postResponse.id").type(JsonFieldType.NUMBER).description("게시글 식별자"),
-                fieldWithPath("postResponse.title").type(JsonFieldType.STRING).description("게시글 제목"),
-                fieldWithPath("postResponse.content").type(JsonFieldType.STRING).description("게시글 내용"),
-                fieldWithPath("postResponse.category").type(JsonFieldType.STRING).description("카테고리"),
-                fieldWithPath("postResponse.price").type(JsonFieldType.NUMBER).description("가격"),
-                fieldWithPath("postResponse.desiredPlaceName").type(JsonFieldType.STRING)
-                    .description("거래희망 장소 이름").optional(),
-                fieldWithPath("postResponse.desiredPlaceLongitude").type(JsonFieldType.NUMBER)
-                    .description("거래희망 장소 경도").optional(),
-                fieldWithPath("postResponse.desiredPlaceLatitude").type(JsonFieldType.NUMBER)
-                    .description("거래희망 장소 위도").optional(),
-                fieldWithPath("postResponse.view").type(JsonFieldType.NUMBER).description("조회수"),
-                fieldWithPath("postResponse.sharing").type(JsonFieldType.BOOLEAN).description("나눔 여부"),
-                fieldWithPath("postResponse.townName").type(JsonFieldType.STRING)
-                    .description("거래 기준 동네"),
-                fieldWithPath("postResponse.statusType").type(JsonFieldType.STRING)
-                    .description("거래 상태"),
-                fieldWithPath("postResponse.likeCount").type(JsonFieldType.NUMBER)
-                    .description("좋아요 개수"),
-                fieldWithPath("memberResponse.id").type(JsonFieldType.NUMBER).description("작성자 식별자"),
-                fieldWithPath("memberResponse.profileImgUrl").type(JsonFieldType.STRING)
-                    .description("작성자 프로필 이미지 url").optional(),
-                fieldWithPath("memberResponse.nickName").type(JsonFieldType.STRING)
-                    .description("작성자 닉네임"),
-                fieldWithPath("imageUrls").type(JsonFieldType.ARRAY).description("게시글 이미지 url 리스트")
-                    .optional())));
-  }
-
-  @Test
   @DisplayName("사용자는 postId, member, town, view를 제외한 모든 값을 수정할 수 있다.")
   void updatePostTest() throws Exception {
     String updateTitle = "updateTitle";
@@ -418,7 +380,7 @@ class PostControllerTest {
   @DisplayName("라이크 버튼을 눌러서 좋아요가 가능하다.")
   void clickLikesTest() throws Exception {
 
-    mockMvc.perform((RestDocumentationRequestBuilders.patch("/posts/likes/{id}", postId)
+    mockMvc.perform((RestDocumentationRequestBuilders.patch("/posts/{id}/likes", postId)
             .header("AccessToken", accessToken).contentType(MediaType.APPLICATION_JSON)))
         .andExpect(status().isOk()).andDo(MockMvcResultHandlers.print()).andDo(
             document("post/api/patch/updateLikes", preprocessRequest(prettyPrint()),

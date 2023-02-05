6fedbb9c104c78cc03a4f10326257045f4e86723
Albatross3 <superstring77@gmail.com>
2023-02-05 16:53:53 +0900

    test : ChatMessage 테스트 코드 추가
    - 채팅 메세지 저장 실패 경우에 대해 테스트 코드 추가하였습니다 (채팅방을 찾지 못하는 경우에 대하여)

--- src/test/java/com/dangdang/server/domain/message/application/ChatMessageServiceTest.java
+++ src/test/java/com/dangdang/server/domain/message/application/ChatMessageServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.dangdang.server.domain.chatroom.domain.ChatRoomRepository;
 import com.dangdang.server.domain.chatroom.domain.entity.ChatRoom;
+import com.dangdang.server.domain.chatroom.exception.ChatRoomNotFoundException;
 import com.dangdang.server.domain.member.domain.MemberRepository;
 import com.dangdang.server.domain.member.domain.entity.Member;
 import com.dangdang.server.domain.message.domain.MessageRepository;
@@ -20,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @SpringBootTest
 class ChatMessageServiceTest {
@@ -67,27 +67,42 @@ class ChatMessageServiceTest {
     ChatMessage chatMessage1 = new ChatMessage(savedChatRoom.getId(), savedBuyer.getNickname(), "안녕하세요 당근당근");
     ChatMessage chatMessage2 = new ChatMessage(savedChatRoom.getId(), savedBuyer.getNickname(), "에어팟 팔렸나요?");
 
-    // when
     chatMessageService.saveChatMessage(chatMessage1);
     chatMessageService.saveChatMessage(chatMessage2);
 
-    // then
     List<Message> messages = messageRepository.findByChatRoomIdOrderByCreatedAt(savedChatRoom.getId());
     assertThat(messages.size()).isEqualTo(2);
     for (Message message : messages) {
       System.out.println(message.getMessage());
     }
-
-
-
   }
+
   @Test
-  void print() {
-    ChatRoom chatRoom = chatRoomRepository.findById(49L).get();
-    List<Message> messageList = chatRoom.getMessageList();
-    System.out.println(messageList.size());
-    for (Message message : messageList) {
-      System.out.println(message.getMessage());
-    }
+  @DisplayName("채팅 메세지 저장 실패 - 채팅방을 찾을 수 없는 경우")
+  @Transactional
+  void saveChatMessage_failByNotFoundChatRoom() {
+    Member buyer = new Member("01012345677", "buyer");
+    Member seller = new Member("01087654321", "seller");
+    Town sellerTown = new Town("내동네", null, null);
+
+    Post post = new Post("에어팟", "에어팟 팝니다", Category.디지털기기, null,
+        null, null, null,
+        0, false, seller, sellerTown, null, null);
+
+    ChatRoom chatRoom = new ChatRoom(buyer, seller, post);
+
+    Member savedBuyer = memberRepository.save(buyer);
+    memberRepository.save(seller);
+    townRepository.save(sellerTown);
+    postRepository.save(post);
+    chatRoomRepository.save(chatRoom);
+
+    ChatMessage chatMessage = new ChatMessage(0L, savedBuyer.getNickname(), "안녕하세요 당근당근");
+
+    assertThatThrownBy(() -> chatMessageService.saveChatMessage(chatMessage))
+        .isInstanceOf(ChatRoomNotFoundException.class);
+
+
   }
+
 }
